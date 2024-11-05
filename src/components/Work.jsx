d21e4211b0fb63da02dc746154ef4819bedc09b6
Akash Kumar B <akashbkumar30@gmail.com>
2024-11-05 13:57:08 +0530

    Final Commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,41 +2,17 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
+    imgSrc: '/images/project-1.jpeg',
+    title: 'Food Order App',
     tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
+    projectLink: 'https://food-app-frontend-steel.vercel.app/'
   },
   {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://pixstock-official.vercel.app/'
-  },
-  {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
-  },
-  {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
-  },
-  {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-  },
-  {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-  },
+    imgSrc: '/images/project-2.png',
+    title: 'Movie Rating App app',
+    tags: ['API', 'Role-Based Access'],
+    projectLink: 'https://github.com/AKASHBKUMAR/movie-rating-app'
+  }
 ];
 
 const Work = () => {
